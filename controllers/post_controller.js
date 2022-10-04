d8cdd15ceae1203bd9f56e03524a5d161ffe0bcd
networkdavit <lonecreation1612@gmail.com>
2022-10-05 00:54:47 +0400

    Authoriza post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -11,8 +11,6 @@ function authenticateToken(req, res, next) {
 	if (token == null) return res.sendStatus(401)
   
 	jwt.verify(token, SECRET, (err, user) => {
-	  console.log(err)
-  
 	  if (err) return res.sendStatus(403)
   
 	  req.user = user
--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ function post_routes(app){
 
     app.get('/post/:id',post_controller.authenticateToken, post_controller.get_one_post)
 
-    app.post('/post', post_controller.make_post)
+    app.post('/post', post_controller.authenticateToken ,post_controller.make_post)
 }
 
 module.exports = {post_routes}
